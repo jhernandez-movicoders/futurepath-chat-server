748843cdf04d23225af8e97500fc734009fe9085
Angela Serrano <aserranocu@gmail.com>
2020-01-10 19:40:42 +0100

    Innited project in netbeans

---
+++ src/futurepath/chat/server/FuturepathChatServer.java
@@ -0,0 +1,14 @@
+
+package futurepath.chat.server;
+
+
+public class FuturepathChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
