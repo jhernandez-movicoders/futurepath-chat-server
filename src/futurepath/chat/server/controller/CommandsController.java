a02649b9bbbf4907bfcbd73aa8af7a1959f0ce15
Salahdin Belghiti <salahdin.belghiti@movicoders.com>
2020-01-18 13:06:48 +0100

    Metodos de interpretacion de mensajes terminados

---
+++ src/futurepath/chat/server/controller/CommandsController.java
@@ -0,0 +1,142 @@
+package futurepath.chat.server.controller;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class CommandsController { private int numrooms;
+    private int usrsxroom;
+    private ServerSocket serverSocket;
+    private HashMap<Socket, String> usersConnected;
+    private HashMap<String, ArrayList<String>> usersInTheRoom; 
+    private ArrayList<Thread> threads;
+    private static final int PORT = 8080;
+    
+    private void interpret(String[] args, Socket socket) {
+        String username = usersConnected.get(socket);
+        switch (args[0]) {
+            case "CONNECT":
+                if(userExists(username)) {
+                    respond("Nombre de usuario ya existe.", socket);
+                } else {
+                    startThread(username, socket);
+                }
+                break;
+            case "JOIN":
+                String room = args[1];
+                if(usersInTheRoom.containsKey(room)) {
+                    if(usersInTheRoom.containsValue(username)) {
+                        respond("Ya estas dentro de este room", socket);
+                    } else {
+                        usersInTheRoom.get(room).add(username);
+                    }
+                } else {
+                    if(usersInTheRoom.size() < numrooms) {
+                        ArrayList<String> users = new ArrayList<String>();
+                        users.add(username);
+                        usersInTheRoom.put(room, users);
+                    } else {
+                        respond("No existe este room y no se puede crear porque se ha alcanzado el limite de rooms.",socket);
+                    }
+                }
+                break;
+            case "LEAVE":
+                if(usersInTheRoom.containsKey(args[1])) {
+                    usersInTheRoom.get(args[1]).remove(username);
+                    if(usersInTheRoom.get(args[1]).isEmpty()) {
+                        usersInTheRoom.remove(args[1]);
+                    }
+                }
+                break;
+            case "SEND":
+                String[] messageArray = new String[args.length-2];
+                String message = String.join(",", messageArray);
+                if(usersInTheRoom.containsKey(args[1])) {
+                    ArrayList<Socket> socketsToSend = getSocketsOfClients(usersInTheRoom.get(args[1]));
+                    for(int i = 0; i < socketsToSend.size(); i++) {
+                        respond(message,socketsToSend.get(i));
+                    }
+                } else {
+                    respond("Estas enviando mensaje a un room que no existe.", socket);
+                }
+                break;
+            case "DISCONNECT":
+                if(userExists(username)) {
+                    try {
+                        usersConnected.remove(socket);
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    respond("No estas conectado.", socket);
+                }
+                break;
+        }
+    }
+    private ArrayList<Socket> getSocketsOfClients(ArrayList<String> users) {
+        ArrayList<Socket> sockets = new ArrayList<>();
+        usersConnected.forEach((socket, user) -> {
+            if(users.contains(user))
+                sockets.add(socket);
+        });
+        return sockets;
+    }
+    private void updateRoomsClient(Socket socket) {
+        try {
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            for(int i = 0; i < usersInTheRoom.size(); i++) {
+                String message = "ROOM" + " " + usersInTheRoom.get(i);
+                out.writeUTF(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateClientsInRoom(String room, Socket socket) {
+        try {
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            ArrayList<String> users = usersInTheRoom.get(room);
+            for(int i = 0; i < users.size(); i++) {
+                String message = "USERS" + " " + users.get(i);
+                out.writeUTF(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void respond(String message, Socket socket){
+        try {
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startThread(String username, Socket socket) {
+    try {
+        usersConnected.put(socket, username);
+        updateRoomsClient(socket);
+        Thread thread = new Connection(1,1);
+        threads.add(thread);
+    } catch (IOException ex) {
+        Logger.getLogger(CommandsController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }
+
+    private boolean userExists(String name) {
+        if(usersConnected.containsValue(name)) {
+            return true;
+        }
+        return false;
+    }
+}
